7a29653447e89db1d81a26785270a672b7470295
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2019-05-12 10:52:40 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -26,24 +26,9 @@ echo '2.4 создание разделов'
   echo;
   echo;
   echo;
-  echo +100M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
   echo +20G;
 
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +1024M;
 
-  echo n;
-  echo p;
-  echo;
-  echo;
   echo a;
   echo 1;
 
@@ -54,17 +39,12 @@ echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L boot
-mkfs.ext4  /dev/sda2 -L root
-mkswap /dev/sda3 -L swap
-mkfs.ext4  /dev/sda4 -L home
+mkfs.ext4  /dev/sda1 -L root
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda2 /mnt
-mkdir /mnt/{boot,home}
+mount /dev/sda1 /mnt
+mkdir /mnt/{home}
 mount /dev/sda1 /mnt/boot
-swapon /dev/sda3
-mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 
