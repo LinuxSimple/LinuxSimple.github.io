b812c43cffca5574514b3b59cc77ab3fe2ec6a87
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2019-05-12 17:09:20 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-rm -rf ~/.config/xfce4/*
-mkdir ~/Downloads
-cd ~/Downloads
 
-echo 'Установка AUR (yay)'
-sudo pacman -Syu
-sudo pacman -S wget --noconfirm
-wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+# Установка "yay" из AUR
+# echo 'Установка YAY (yaourt)'
+#sudo pacman -Syu
+#mkdir -p /tmp/yay_install
+#cd /tmp/yay_install
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -sir --needed --noconfirm --skippgpcheck
+#rm -rf yay_install
+
+# Установка pamac (установщик пакетов из AUR c GUI)
+#echo 'Установка PAC (pamac)'
+#sudo pacman -Syu
+#mkdir -p /tmp/pamac-aur_install
+#cd /tmp/pamac-aur_install
+#git clone https://aur.archlinux.org/pamac-aur.git
+#cd pamac-aur
+#makepkg -si --needed --noconfirm --skippgpcheck
+#rm -rf pamac-aur_install
