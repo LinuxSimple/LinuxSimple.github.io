95964f88ac1be6c0a4d926934040e3127287853d
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2019-08-04 15:47:08 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -56,4 +56,4 @@ pacstrap /mnt base base-devel
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://github.com/LinuxSimple/LinuxSimple.github.io/blob/master/arch2.sh)"
