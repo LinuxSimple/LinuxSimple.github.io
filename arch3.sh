fa536c3aa9546ee9a59110e51626204c46853938
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2020-03-24 19:46:08 +0200

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -9,13 +9,3 @@
 #cd yay
 #makepkg -sir --needed --noconfirm --skippgpcheck
 #rm -rf yay_install
-
-# Установка pamac (установщик пакетов из AUR c GUI)
-#echo 'Установка PAC (pamac)'
-#sudo pacman -Syu
-#mkdir -p /tmp/pamac-aur_install
-#cd /tmp/pamac-aur_install
-#git clone https://aur.archlinux.org/pamac-aur.git
-#cd pamac-aur
-#makepkg -si --needed --noconfirm --skippgpcheck
-#rm -rf pamac-aur_install
