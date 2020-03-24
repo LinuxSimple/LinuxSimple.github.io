ee3409bb5c1d8cb6881dd666009e9fa1bac7c516
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2020-03-24 19:16:12 +0200

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -24,7 +24,17 @@ echo;
 
 echo;
 
-echo +40G;
+echo +20G;
+
+echo n;
+
+echo;
+
+echo;
+
+echo;
+
+echo +250G;
 
 echo w;
 
@@ -38,9 +48,13 @@ echo '2.4.2 Форматирование дисков'
 
 mkfs.ext4 /dev/sda1 -L root
 
+mkfs.ext4 /dev/sda2 -L home
+
 echo '2.4.3 Монтирование дисков'
 
 mount /dev/sda1 /mnt
+
+mount /dev/sda2 /mnt
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало'
 
