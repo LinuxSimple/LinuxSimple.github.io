e6ead8221ce956f6ac68c9f80ea1e669e610fa44
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2020-06-20 20:47:53 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -21,7 +21,7 @@ echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
 echo 'Создадим загрузочный RAM диск'
-mkinitcpio -P linux
+mkinitcpio -p linux
 
 echo '3.5 Устанавливаем загрузчик'
 pacman -Syy
