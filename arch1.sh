dba332baa3a07b2d3fa0dd4d2ae4b9c94690d14d
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2020-07-06 15:31:28 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -6,37 +6,13 @@ echo '2.3 Синхронизация системных часов'
 
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
-
-(
-
-echo o;
-
-echo n;
-
-echo;
-
-echo;
-
-echo;
-
-echo +50G;
-
-echo w;
-
-) | fdisk /dev/sda
-
-echo 'Ваша разметка диска'
-
-fdisk -l
-
 echo '2.4.2 Форматирование дисков'
 
-mkfs.ext4 /dev/sda1 -L root
+mkfs.btrs /dev/sda4 -L root
 
 echo '2.4.3 Монтирование дисков'
 
-mount /dev/sda1 /mnt
+mount /dev/sda4 /mnt
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало'
 
