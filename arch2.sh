6eaf9e3f07612e30b8d8414bf400069c20a9b3b7
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2021-05-10 21:30:28 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -68,7 +68,7 @@ if [[ $vm_setting == 1 ]]; then
   pacman -S xfce4 xfce4-goodies xdg-user-dirs --noconfirm
 elif [[ $vm_setting == 2 ]]; then
   pacman -S plasma-desktop sddm-kcm powerdevil konsole plasma-pa
- dolphin --noconfirm
+ dolphin kwalletmanager --noconfirm
 elif [[ $vm_setting == 3 ]]; then  
   pacman -S  lxqt
 elif [[ $vm_setting == 4 ]]; then
