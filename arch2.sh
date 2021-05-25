afa04f422489ef9b239cd28f28f9224a49757e11
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2021-05-25 11:21:37 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -103,8 +103,7 @@ echo 'Подключаем автозагрузку менеджера вход�
 systemctl enable NetworkManager
 
 echo 'Настраиваем звук и необходимые программы'
-pacman -S pulseaudio --noconfirm
-pacman -S pavucontrol --noconfirm
+pacman -S pipewire --noconfirm
 pacman -S htop gvfs-mtp mtpfs nano p7zip unrar wget git gtk2 noto-fonts-emoji xdg-user-dirs --noconfirm
 
 echo 'CИСТЕМА УСТАНОВЛЕНА, ПЕРЕЗАГРУЗИТЕ КОМПЬЮТЕР'
