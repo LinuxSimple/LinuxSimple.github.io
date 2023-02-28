49900d848d544a18e6459ae45a653722a6409de0
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2023-02-28 16:22:07 +0200

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -67,7 +67,7 @@ read -p "1 - XFCE, 2 - KDE, 3 - Lxqt, 4 - deepin, 5 - gnome 6 - cinnamon: " vm_s
 if [[ $vm_setting == 1 ]]; then
   pacman -S xfce4 xfce4-goodies xdg-user-dirs --noconfirm
 elif [[ $vm_setting == 2 ]]; then
-  pacman -S plasma-desktop sddm-kcm powerdevil konsole plasma-pa --noconfirm
+  pacman -S plasma sddm-kcm  konsole plasma-pa --noconfirm
 elif [[ $vm_setting == 3 ]]; then  
   pacman -S lxqt breeze-icons
 elif [[ $vm_setting == 4 ]]; then
