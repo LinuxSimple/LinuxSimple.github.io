d2a5d1281b356d5f009a4c1e02e46578f06f3e21
LinuxSimple <50518752+LinuxSimple@users.noreply.github.com>
2025-10-30 17:25:47 +0200

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -43,8 +43,7 @@ echo 'Устанавливаем пароль пользователя'
 passwd $username
 
 echo 'Устанавливаем SUDO'
-#echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
-echo '%wheel ALL=\(ALL\) ALL' >> /etc/sudoers
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
 echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
